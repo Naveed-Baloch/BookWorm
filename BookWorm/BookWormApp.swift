109707c5ee6225bb81ea44197fff627452f9b99e
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-26 20:15:13 +0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Naveed on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
